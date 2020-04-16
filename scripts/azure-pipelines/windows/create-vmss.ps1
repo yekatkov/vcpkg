f662cdaddf44719cc6d427b8422f8ccc4a3a9ce0
Billy Robert O'Neal III <bion@microsoft.com>
2020-04-15 20:53:25 -0700

    Change to Standard_D16as_v4.

--- scripts/azure-pipelines/windows/create-vmss.ps1
+++ scripts/azure-pipelines/windows/create-vmss.ps1
@@ -8,7 +8,7 @@
 
 $Location = 'westus2'
 $Prefix = 'PrWin-' + (Get-Date -Format 'yyyy-MM-dd')
-$VMSize = 'Standard_F16s_v2'
+$VMSize = 'Standard_D16as_v4'
 $ProtoVMName = 'PROTOTYPE'
 $LiveVMPrefix = 'BUILD'
 $WindowsServerSku = '2019-Datacenter'
